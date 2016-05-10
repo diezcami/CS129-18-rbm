e3e192a05231afa1bcb0305a4beafc91352c1c8a
Cami Diez <diezcami@gmail.com>
2016-05-10 15:42:37 +0800

    Added vector to image function

--- process.py
+++ process.py
@@ -1,4 +1,6 @@
 import cv2
+import os
+import numpy as np
 
 INPUT_DIR = 'input/'
 
@@ -17,12 +19,26 @@ def get_bipolar_vector (img):
                 bipolar_vector.append(1)
     return bipolar_vector
 
+# Input: Bipolar Vector
+# Output: Image (Matrix)
+def get_image (bipolar_vector):
+    image_vector = [[0 for i in range(300)] for j in range(450)]
+    bv_index = 0 
+    for row in range(len(image_vector)):
+        for col in range(len(image_vector[0])):
+            if bipolar_vector[bv_index] == 1:
+                image_vector[row][col] = 255
+            else:
+                image_vector[row][col] = 1
+            bv_index += 1
+    return image_vector 
+
 # Input: Directory of Images
 # Output: Array of images inside the directory
 def load_images_from_folder (folder):
     images = []
     for filename in os.listdir(folder):
-        img = cv2.imread(os.path.join(folder,filename))
+        img = cv2.imread(os.path.join(folder,filename), 0)
         if img is not None:
             images.append(img)
     return images
@@ -32,3 +48,8 @@ if __name__ == '__main__':
 
     for image in images:
         bp = get_bipolar_vector (image)
+        new_image = get_image (bp)
+        cv2.imshow('black and white',np.array(new_image))
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
--- runner.py
+++ runner.py
@@ -1,9 +1,10 @@
 from bam import *
 from process import *
 
+
 if __name__ == '__main__':
     images = load_images_from_folder (INPUT_DIR)
-    
+
     for image in images:
         bp = get_bipolar_vector (image)
         # feed bp to BAM here
