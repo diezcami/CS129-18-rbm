83e71f601d71beb3a5456250f564b79cce19814c
Cami Diez <diezcami@gmail.com>
2016-05-10 23:42:15 +0800

    Removed partial camel case

--- bam.py
+++ bam.py
@@ -2,62 +2,62 @@ import random as rand
 import numpy as np
 import math as m
 
-class bam:
+class Bam:
 	def __init__(self, n, p, random=False):
 		self.energy = self.mean = 0.0
 		self.stdev = 0.1
 		
-		self.rowCount = n
-		self.colCount = p
-		self.weightMatrix = self.makeNewWeightMatrix(random)
+		self.row_count = n
+		self.col_count = p
+		self.weight_matrix = self.make_new_weight_matrix(random)
 		
-	def feedForward(self, input, stochastic=True):
-		result = ( np.mat(input) * np.mat(self.weightMatrix) ).tolist()[0]
+	def feed_forward(self, input, stochastic=True):
+		result = ( np.mat(input) * np.mat(self.weight_matrix) ).tolist()[0]
 		if stochastic:
-			result = map(lambda x: 1 if self.logistic(x) > self.randomGaussian() else -1, result)
+			result = map(lambda x: 1 if self.logistic(x) > self.random_gaussian() else -1, result)
 		else:
 			result = map(lambda x: 1 if x>0 else -1, result)
 		return result
 	
-	def feedBackward(self, output, stochastic=True):
-		result = ( np.mat(output) * np.mat(zip(*self.weightMatrix)) ).tolist()[0]
+	def feed_backward(self, output, stochastic=True):
+		result = ( np.mat(output) * np.mat(zip(*self.weight_matrix)) ).tolist()[0]
 		if stochastic:
-			result = map(lambda x: 1 if self.logistic(x) > self.randomGaussian() else -1, result)
+			result = map(lambda x: 1 if self.logistic(x) > self.random_gaussian() else -1, result)
 		else:
 			result = map(lambda x: 1 if x>0 else -1, result)
 		return result
 		
 	# note: the source code of sir happy originally said
-	#   weightMatrix[r][c]input.at(r) * input.at(c)
+	#   weight_matrix[r][c]input.at(r) * input.at(c)
 	#   it was clarified na dapat output yung isa
-	def computeEnergy(self, input, output):
+	def compute_energy(self, input, output):
 		e = 0.0
 		for r in range(len(input)):
 			for c in range(len(output)):
-				e += self.weightMatrix[r][c] * input[r] * output[c]
+				e += self.weight_matrix[r][c] * input[r] * output[c]
 		self.energy = -1 * e
 	
 	def train(self, input, output):
-		newWeightMatrix = np.mat(self.weightMatrix)
+		new_weight_matrix = np.mat(self.weight_matrix)
 		for pair in zip(input, output):
 			m2 = np.mat(zip(pair[0])) * np.mat(pair[1])
-			newWeightMatrix += m2
-		self.weightMatrix = newWeightMatrix.tolist()
+			new_weight_matrix += m2
+		self.weight_matrix = new_weight_matrix.tolist()
 		
 	def logistic(self, x):
 		return float("inf") if x==0 else 1.0/(1.0-(m.exp(-x)))
 
-	def randomGaussian(self):
+	def random_gaussian(self):
 		return np.random.normal(self.mean, self.stdev)
 	
-	def makeNewWeightMatrix(self, random=False):
+	def make_new_weight_matrix(self, random=False):
 		if not random:
-			return [ [0.0]*self.colCount for i in range(self.rowCount) ]
+			return [ [0.0]*self.col_count for i in range(self.row_count) ]
 		else:
-			return [ [rand.randrange(-1, 1) for i in range(self.colCount)] for j in range(self.rowCount)]
+			return [ [rand.randrange(-1, 1) for i in range(self.col_count)] for j in range(self.row_count)]
 	
-	def printWeightMatrix(self):
-		print np.mat(self.weightMatrix)
+	def print_weight_matrix(self):
+		print np.mat(self.weight_matrix)
 		
-	def getEnergy(self):
+	def get_energy(self):
 		return self.energy
--- driver.py
+++ driver.py
@@ -2,12 +2,12 @@ import cv2
 from bam import *
 from process import *
 
-numRows = 135000
-numCols = 60
+num_rows = 135000
+num_cols = 60
 
 if __name__ == '__main__':
     images = load_images_from_folder (INPUT_DIR)
-    b = bam(numRows, numCols, True)
+    b = Bam(num_rows, num_cols, True)
     
     for image in images:
         bp = get_bipolar_vector(image)
@@ -17,11 +17,11 @@ if __name__ == '__main__':
         
         for i in range(epoch):
             # bp is the testData
-            bp_prime = b.feedForward(bp, False)
-            bp_new = b.feedBackward(bp_prime, False)
+            bp_prime = b.feed_forward(bp, False)
+            bp_new = b.feed_backward(bp_prime, False)
             
             print "=================="
-            b.computeEnergy(bp_new, bp_prime)
-            print "Energy: "+str(b.getEnergy())
+            b.compute_energy(bp_new, bp_prime)
+            print "Energy: "+str(b.get_energy())
             print "=================="
             bp = bp_new
