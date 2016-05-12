97a9e250150943eaf90fd7fe68e762c4694abc63
Andre T. Tan <d_tan_man@yahoo.com>
2016-05-12 21:27:16 +0800

    driver.py will now take all pics in input dir to train

--- driver.py
+++ driver.py
@@ -8,17 +8,20 @@ num_cols = 60
 if __name__ == '__main__':
     start_time = time.time()
     images = load_images_from_folder (INPUT_DIR)
-    
-    # True indicates if a random weight matrix will be initialized.
-    b = bam(num_rows, num_cols)
-    label_of_apple = get_last_label (True, 25) # Uncomment for more general usage
-    print "Finished retrieving last label"
+    image_counter = 0
 
-    filecount = 0
+    # ---- training ----------------------------------------
     for image in images:
+        print "starting with image no. "+str(image_counter)
         bp = get_bipolar_vector(image)
+        
+        # True indicates if a random weight matrix will be initialized.
+        b = bam(num_rows, num_cols)
+        label_of_apple = get_last_label (True, 25) # Uncomment for more general usage
+        
+        print "Finished retrieving last label"
 		
-        convert_to_image(bp, 'img '+count)
+        convert_to_image(bp, 'img 1')
 		
         total_differences = 0
         total_distance = 0.0
@@ -28,18 +31,16 @@ if __name__ == '__main__':
         inputs_to_train = [bp]
         outputs_to_train = [label_of_apple]
         
-        while True:
-            print "now at iteration "+str(iter_count)
+        while iter_count <= max_iter_count:
+            print "now at iteration "+str(iter_count)+" of training"
             b.train(inputs_to_train, outputs_to_train)
-            
-            if iter_count==max_iter_count:
-                print "--- --- --- --- --- --- --- --- --- --- ---"
-                print "Max number of iterations reached! Stopping."
-                print "Check line 14 of driver.py to change."
-                print "--- --- --- --- --- --- --- --- --- --- ---"
-                break
-            else:
-                iter_count += 1
-            print ""
+            iter_count += 1
+        
+        print "--- --- --- --- --- --- --- --- --- --- ---"
+        print "Max number of iterations reached! Stopping."
+        print "Check line 28 of driver.py to change."
+        print "--- --- --- --- --- --- --- --- --- --- ---"
         
         print "program execution: "+str(time.time()-start_time)
+        
+    # ---- part 1: supervised learning ---------------------
